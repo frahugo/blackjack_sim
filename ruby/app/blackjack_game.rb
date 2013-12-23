3b7622d0ee9d51ac44dcaf938c879cc45ef06105
Hugo Frappier <hugo@cekoya.com>
2013-12-23 12:22:33 -0500

    Change blackjack detection logic + add log method

--- ruby/app/blackjack_game.rb
+++ ruby/app/blackjack_game.rb
@@ -4,7 +4,7 @@ require File.dirname(__FILE__) + "/util/" + 'strategy_util.rb'
 require 'yaml'
 
 class BlackjackGame
- 
+
   attr_accessor :num_losses,:num_wins,:num_pushes,:total_winnings
 
   def initialize num_hands, bet_size, shoe_size
@@ -16,21 +16,29 @@ class BlackjackGame
     @num_wins = 0
     @num_pushes = 0
     @total_winnings = 0
+    @highest_winnings = 0
+    @lowest_winnings = 0
   end
- 
+
   def play_game
     @num_hands.times  {
-      dealerHand = BlackjackHand.new @shoe.deal_card, @shoe.deal_card  
+      dealerHand = BlackjackHand.new @shoe.deal_card, @shoe.deal_card
       playerHand = BlackjackHand.new @shoe.deal_card, @shoe.deal_card
-      
+
       play_hand(dealerHand, playerHand,@bet_size)
-    
+      log "Winnings: #{@total_winnings}"
+      log "------------------------------------------------------------"
+
       @shoe.burn_cards
     }
   end
-  
+
   def to_s
-    "NumWins: [#{@num_wins}], NumLosses: [#{@num_losses}], NumPushes: [#{@num_pushes}], TotalWinnings: [#{@total_winnings}] \n Win/Loss: [#{@num_wins/@num_losses.to_f}]"
+    "NumWins: [#{@num_wins}], NumLosses: [#{@num_losses}], NumPushes: [#{@num_pushes}]\nTotalWinnings: [#{@total_winnings}] Min: [#{@lowest_winnings}] Max: [#{@highest_winnings}]\nWin/Loss: [#{@num_wins/@num_losses.to_f}]"
+  end
+
+  def log message
+    puts message
   end
 
   def dp dealerHand, playerHand
@@ -40,68 +48,77 @@ class BlackjackGame
   def dps dealerHand, playerHand
     "Playerhand #{playerHand}=#{playerHand.hand_value}  Dealerhand: #{dealerHand.hand[0]}"
   end
+
   private
+
   def play_hand dealerHand, playerHand, curr_bet_size
-    #puts "Playing hand... #{dps dealerHand, playerHand}"
+    log "Playing hand... #{dps dealerHand, playerHand}"
     show_card = dealerHand.hand[0]
 
     if (playerHand.hand_value > 21)
-      #puts "BUSTED! #{dp dealerHand, playerHand}" 
+      log "BUSTED! #{dp dealerHand, playerHand}"
       @num_losses = @num_losses + 1
       @total_winnings = @total_winnings - curr_bet_size
-      return nil
-    elsif playerHand.hand_value == 21 && playerHand.hand.count == 2
-      if dealerHand.hand_value == 21
-        #puts "PUSH! #{dp dealerHand, playerHand}"
+      return
+    elsif playerHand.blackjack?
+      if dealerHand.blackjack?
+        log "PUSH! #{dp dealerHand, playerHand}"
         @num_pushes = @num_pushes + 1
-      elsif
-        #puts "BLACKJACK! #{dp dealerHand, playerHand}"
+      else
+        log "BLACKJACK! #{dp dealerHand, playerHand}"
         @num_wins = @num_wins + 1
         @total_winnings = @total_winnings + (curr_bet_size * 3/2.to_f)
       end
-      return nil
+      return
+    elsif dealerHand.blackjack?
+      log "DEALER HAS BLACKJACK! #{dp dealerHand, playerHand}"
+      @num_losses = @num_losses + 1
+      @total_winnings = @total_winnings - curr_bet_size
+      return
     end
-  
-    strat = playerHand.get_strategy show_card,@strategies
-    hand_result = nil 
+
+    strat = playerHand.get_strategy show_card, @strategies
+    hand_result = nil
     if (strat == :split)
-      #puts "SPLITTING #{dps dealerHand, playerHand}"
+      log "SPLITTING #{dps dealerHand, playerHand}"
       play_hand(dealerHand, BlackjackHand.new(playerHand.hand[0], @shoe.deal_card), curr_bet_size)
-      play_hand(dealerHand, BlackjackHand.new(playerHand.hand[1], @shoe.deal_card), curr_bet_size) 
+      play_hand(dealerHand, BlackjackHand.new(playerHand.hand[1], @shoe.deal_card), curr_bet_size)
     elsif strat == :double
-      #puts "DOUBLING"
-      playerHand << @shoe.deal_card 
-      curr_bet_size = curr_bet_size * 2
-      hand_result = finish_hand dealerHand, playerHand 
-    elsif strat == :hit
-      #puts "HITTING #{dps dealerHand,playerHand}"
+      log "DOUBLING"
       playerHand << @shoe.deal_card
-      play_hand(dealerHand,playerHand,curr_bet_size)
+      curr_bet_size = curr_bet_size * 2
+      hand_result = finish_hand(dealerHand, playerHand)
+    elsif strat == :hit
+      log "HITTING #{dps dealerHand,playerHand}"
+      playerHand << @shoe.deal_card
+      play_hand(dealerHand, playerHand, curr_bet_size)
     elsif strat == :stay
-      #puts "STAY #{dps dealerHand, playerHand}"
-      hand_result = finish_hand dealerHand, playerHand
+      log "STAY #{dps dealerHand, playerHand}"
+      hand_result = finish_hand(dealerHand, playerHand)
+    else
+      raise "Something's wrong #{dp dealerHand, playerHand}"
     end
 
     if (hand_result == :win)
-      #puts "WIN! #{dp dealerHand, playerHand}"
-      @num_losses = @num_losses + 1
-      @total_winnings = @total_winnings - curr_bet_size
-    elsif hand_result == :loss
-      #puts "LOSS! #{dp dealerHand, playerHand}"
+      log "WIN! #{dp dealerHand, playerHand}"
       @num_wins = @num_wins + 1
+      @total_winnings = @total_winnings + curr_bet_size
+    elsif hand_result == :loss
+      log "LOSS! #{dp dealerHand, playerHand}"
       @num_losses = @num_losses + 1
       @total_winnings = @total_winnings - curr_bet_size
     elsif hand_result == :push
-      #puts "PUSH! #{dp dealerHand, playerHand}"
+      log "PUSH! #{dp dealerHand, playerHand}"
       @num_pushes = @num_pushes + 1
     end
 
-    #puts "------------------------------------------------------------"
+    @highest_winnings = [@total_winnings, @highest_winnings].max
+    @lowest_winnings = [@total_winnings, @lowest_winnings].min
   end
 
   def finish_hand dealerHand, playerHand
     #deal to dealer to hard 17
-    until dealerHand.hand_value >= 17 && !dealerHand.is_soft? 
+    until dealerHand.hand_value >= 17 && !dealerHand.is_soft?
       dealerHand << @shoe.deal_card
     end
 
