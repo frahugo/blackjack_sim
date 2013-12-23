393769353ed5b7e5821c5dafaf23ece2233c3a81
Hugo Frappier <hugo@cekoya.com>
2013-12-23 12:40:16 -0500

    Add a marker in the shoe (last 20%)

--- ruby/app/models/shoe.rb
+++ ruby/app/models/shoe.rb
@@ -2,8 +2,8 @@ require File.dirname(__FILE__) + "/" + 'deck.rb'
 
 class Shoe
 
-  attr_accessor :shoe,:dealt_cards,:used_cards
-  
+  attr_accessor :shoe, :dealt_cards, :used_cards
+
 
   #initialize pass in number of decks defaults to 1
   def initialize num_decks = 1
@@ -13,30 +13,29 @@ class Shoe
     num_decks.times do
       @shoe.concat(Deck.new.deck)
     end
-
+    shuffle!
   end
 
   def deal_card
+    shuffle! if @shoe.size < @marker
+
     next_card = @shoe.pop
-    if (next_card.nil?)
-      @used_cards.shuffle!
-      @shoe = @used_cards
-      @used_cards = []
-      next_card = @shoe.pop
-    end
-  
     @dealt_cards << next_card
+
     return next_card
   end
 
-  def burn_cards 
+  def burn_cards
     @used_cards << @dealt_cards
     @used_cards.flatten!
     @dealt_cards = []
   end
 
   def shuffle!
+    @shoe = @used_cards + @shoe
+    @used_cards = []
     @shoe.shuffle!
+    @marker = rand * (@shoe.size / 5)
   end
- 
+
 end
